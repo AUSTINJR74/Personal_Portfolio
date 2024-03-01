8dd238a13c02cc1fb14e8fb27399322d099e65dd
AUSTIN JOSE R <155040982+AUSTINJR74@users.noreply.github.com>
2024-03-01 20:26:37 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll",function() {
-  header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll",()=> {
+  header.classList.toggle ("sticky", window.scrollY >0);
 });
 
 let menu = document.querySelector('#menu-icon');
